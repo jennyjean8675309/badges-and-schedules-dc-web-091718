8063c05b27a642d0754b4e5da443e94a432e4fce
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-30 14:43:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speakers)
+  speakers.each do |name|
+    badge_maker(name)
+end
