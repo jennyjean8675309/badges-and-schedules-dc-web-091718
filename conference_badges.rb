539fb4979f176a47e2b38f0b0d39977151369079
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-30 15:09:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,4 +16,6 @@ def assign_rooms(speakers)
   room_assignments = []
   speakers.each_with_index do |name, index|
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end
+  room_assignments
 end
